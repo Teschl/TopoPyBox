a1effe2264612c185e56c0ef5c3ad943ebee6093
Theo <69400012+Teschl@users.noreply.github.com>
2023-11-19 19:15:13 +0100

    gradient geht erstmal

--- gradient.py
+++ gradient.py
@@ -1,8 +1,23 @@
 import ctypes
+import numpy as np
 
+# Load the C library
+example_lib = ctypes.CDLL('/home/theo/Documents/Praktikum/gradient/lib/build/libgradient.so')
 
-# C Dynamic Link Library
-gradient = ctypes.CDLL("/home/theo/Documents/Praktikum/gradient/lib/build/libgradient.so") #path to .so file
+# Define the argument and return types for the C function
+example_lib.gradient8.argtypes = [
+    ctypes.c_int,    # rows
+    ctypes.c_int,    # cols
+    np.ctypeslib.ndpointer(dtype=np.int32),  # input_matrix
+    np.ctypeslib.ndpointer(dtype=np.int32)   # output_matrix
+]
+example_lib.gradient8.restype = None
 
-gradient.display()
+def calculate_matrix_python(input_matrix):
+    rows, cols = input_matrix.shape
+    output_matrix = np.zeros_like(input_matrix)
 
+    # Call the C function
+    example_lib.gradient8(rows, cols, input_matrix, output_matrix)
+
+    return output_matrix
