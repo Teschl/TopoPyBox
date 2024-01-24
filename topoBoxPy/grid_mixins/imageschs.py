82e22db84dcd6b5f2bac83e73b47c58a288af6f7
Teschl <69400012+Teschl@users.noreply.github.com>
2024-01-24 11:45:53 +0100

    update imageschs

--- topoBoxPy/grid_mixins/imageschs.py
+++ topoBoxPy/grid_mixins/imageschs.py
@@ -1,52 +1,110 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+'''
+.imageschs() plot hillshade image 
+
+is made to emulate it's MATLAB counterpart as best
+as possible using matplotlib. You can plot manually by
+using GRIDobj.z, which is a numpy matrix.
+
+Syntax:
+    DEM.imageschs()
+    DEM.imageschs(pn, pv, ...)
+
+Input:
+    DEM     digital elevation model (GRIDobj)
+
+Parameter/Value Pairs:
+    caxis
+    colorbar        bool,   true (default) to show colorbar
+    colorbarlabel   string, title for colorbar
+
+Output:
+
+'''
 
 class Imageschsmixin:
-    def imagesc(self, *arguments):
+    def imageschs(self, *arguments):
+
+        plt.imshow(self.z, cmap='terrain', interpolation='nearest')
+
         for i, element in enumerate(arguments):
-            if element == "":
-                pass
-            elif element == "caxis":
-                pass
+            if element == "caxis":
+                continue
             elif element == "colorbar":
                 plt.colorbar()
+                continue
             elif element == "colorbarlabel":
-                plt.colorbar(sc, label='Colorbar Label')
+                plt.colorbar(label=arguments[i+1])
+                continue
             elif element == "colorbarylabel":
-                pass
+                continue
             elif element == "colormap":
-                pass
+                continue
             elif element == "percentclip":
-                pass
+                continue
             elif element == "truecolor":
-                pass
+                continue
             elif element == "falsecolor":
-                pass
+                continue
             elif element == "nancolor":
-                pass
+                continue
             elif element == "brighten":
-                pass
+                continue
             elif element == "usepermanent":
-                pass
+                continue
             elif element == "medfilt":
-                pass
+                continue
             elif element == "method":
-                pass
+                continue
             elif element == "azimuth":
-                pass
+                continue
             elif element == "altitude":
-                pass
+                continue
             elif element == "exaggerate":
-                pass
+                continue
             elif element == "ticklabels":
-                pass
+                continue
             elif element == "tickstokm":
-                pass
+                continue
             elif element == "gridmarkers":
-                pass
+                continue
             elif element == "gridmarkercolor":
-                pass
+                continue
 
+            
             plt.show()
             
+
+
+
+
+
+
+'''
+Konzept für später:
+
+def imageschs(self, *arguments):
+    actions = {
+        "caxis": lambda: None,  # Replace with the appropriate action
+        "colorbar": plt.colorbar,
+        "colorbarlabel": lambda label: plt.colorbar(label=label),
+        "colorbarylabel": lambda: None,  # Replace with the appropriate action
+        "colormap": lambda: None,  # Replace with the appropriate action
+        "percentclip": lambda: None,  # Replace with the appropriate action
+        "truecolor": lambda: None,  # Replace with the appropriate action
+        "falsecolor": lambda: None,  # Replace with the appropriate action
+        "nancolor": lambda: None,  # Replace with the appropriate action
+        "brighten": lambda: None,  # Replace with the appropriate action
+        "usepermanent": lambda: None,  # Replace with the appropriate action
+    }
+
+    for i, element in enumerate(arguments):
+        if element in actions:
+            action = actions[element]
+            if len(action.__code__.co_varnames) == 1:  # Check the number of parameters the function takes
+                action(arguments[i+1])
+            else:
+                action()
+'''
