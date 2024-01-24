1819a1f90d3b9701c628434ddae037e22cdc2174
Teschl <69400012+Teschl@users.noreply.github.com>
2024-01-24 15:16:00 +0100

    small fix

--- topoBoxPy/grid_mixins/gradient8.py
+++ topoBoxPy/grid_mixins/gradient8.py
@@ -2,7 +2,6 @@ import ctypes
 import numpy as np
 import copy
 import os
-import time
 
 # find .so file that contains c func
 dirname = os.path.dirname(__file__)
@@ -50,5 +49,5 @@ class Gradient8Mixin:
         # copy object which new z and return
         copy_self = copy.copy(self)
         copy_self.z = output_matrix.copy()
-        print(len(np.unique(output_matrix)))
+        
         return copy_self
