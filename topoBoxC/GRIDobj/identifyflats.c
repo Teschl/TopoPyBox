f0455205a007d1884b0804c9c683f539e6fdab40
Teschl <69400012+Teschl@users.noreply.github.com>
2024-01-25 16:46:35 +0100

    indetifyflats updates

--- topoBoxC/GRIDobj/identifyflats.c
+++ topoBoxC/GRIDobj/identifyflats.c
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <string.h>
 
-void identifyflats(int rows, int cols, float *input, float *flats, float *stills){
+void identifyflats(int rows, int cols, float *input, float *flats, float *sills){
     // Check for flats
     for(int row = 0; row < rows; row++){
         for (int col = 0; col < cols; col++){
@@ -13,15 +13,17 @@ void identifyflats(int rows, int cols, float *input, float *flats, float *stills
 
             for (int x = row-1; x <= row+1; x++){
                 for (int y = col-1; y <= col+1; y++){
-                    // index = x*cols+y
+                    int neighbor = x*cols+y;
+
                     // Skip if out of bounds
                     if(x < 0 || x >= rows || y < 0 || y >= cols) continue; 
                     // Skip if current cell
                     if(x == row && y == col) continue; 
-
-                    if(input[index] <= input[x*cols+y]) tmp ++;
+                    // if neighbor not lower
+                    if(input[index] <= input[neighbor]) tmp ++;
                 }
             }
+            // if all neigbors are not lower
             if(tmp == 8){
                 flats[index] = 1;
             }
@@ -32,27 +34,24 @@ void identifyflats(int rows, int cols, float *input, float *flats, float *stills
         for (int col = 0; col < cols; col++){
             int index = row*cols+col;
 
-            // Skip if cell is flat 
+            // skip if flat at index
             if(flats[index] == 1) continue;
 
-            int tmp = 0;
-            int check = 0;
             for (int x = row-1; x <= row+1; x++){
-                if(flats[row*cols+col] == 1) break;
                 for (int y = col-1; y <= col+1; y++){
-                    int current = x*cols+y;
+                    int neighbor = x*cols+y;
 
                     // Skip if out of bounds
                     if (x < 0 || x >= rows || y < 0 || y >= cols) continue; 
                     // Skip if current cell
-                    if (x == row && y == col) continue; 
-                    if(input[index] > input[current])tmp = 1 ;
-                    if(flats[current] == 1) check = 1;
-                    
+                    if (x == row && y == col) continue;
+
+                    // --------------------
+                    if(flats[neighbor] == 1){
+                        sills[index] = 1;
+                    }
+                    // --------------------
                 }
-            }
-            if (tmp = 1 && check == 1){
-                stills[index] = 1;
             }
         }
     }
