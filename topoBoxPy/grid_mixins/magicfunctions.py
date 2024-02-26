c5ac33f4ecb7bbda88922b7dcd0400d1cfa0b04b
Teschl <69400012+Teschl@users.noreply.github.com>
2024-02-26 14:02:45 +0100

    setup short functions

--- topoBoxPy/grid_mixins/magicfunctions.py
+++ topoBoxPy/grid_mixins/magicfunctions.py
@@ -3,5 +3,77 @@ import copy
 
 
 class MagicFunctionsMixin:
-    def andGrid(self, grid):
+    def andGrid(self, other):
+        pass
+
+    def orGrid(self, other):
+        pass
+
+    def notGrid(self, other):
+        pass
+
+    def all(self, other):
+        pass
+
+    def any(self, other):
+        pass
+
+    def eq(self, other):
+        pass
+
+    def gt(self, other):
+        pass
+
+    def le(self, other):
+        pass
+
+    def lt(self, other):
+        pass
+
+    def log(self, other):
+        pass
+
+    def log2(self, other):
+        pass
+
+    def log10(self, other):
+        pass
+
+    def minus(self, other):
+        pass
+
+    def power(self, other):
+        pass
+
+    def plus(self, other):
+        pass
+
+    def ne(self, other):
+        pass
+
+    def ntimes(self, other):
+        pass
+
+    def mrdivide(self, other):
+        pass
+
+    def sqrt(self, other):
+        pass
+
+    def mpower(self, other):
+        pass
+
+    def zscore(self, other):
+        pass
+
+    def xor(self, other):
+        pass
+
+    def uplus(self, other):
+        pass
+
+    def uminus(self, other):
+        pass
+
+    def times(self, other):
         pass
