dc17cc7dbb480a2d2f1ecbc70d0127d24e4e2325
Teschl <69400012+Teschl@users.noreply.github.com>
2024-03-01 13:22:37 +0100

    GridObject.ge() added

--- topoBoxPy/grid_mixins/magicfunctions.py
+++ topoBoxPy/grid_mixins/magicfunctions.py
@@ -23,9 +23,27 @@ class MagicFunctionsMixin:
 
     def eq(self, other):
         pass
-
+    # greater than
     def gt(self, other):
         pass
+
+    # greater than or equal
+    def ge(self, other):
+        if len(self.z) != len(other.z):
+            raise ValueError("Both DEM must have the same size")
+
+        if(isinstance(other, self.__class__)):
+            z = np.zeros_like(self.z)
+            for i in range(len(self)):
+                if self.z[i] >= other.z[i]:
+                    z[i] == 1
+    
+            result = copy.copy(self)
+            result.z = z
+            return result
+        else:
+            raise ValueError("ge is only supported between instances of DEM.")
+
 
     def le(self, other):
         pass
