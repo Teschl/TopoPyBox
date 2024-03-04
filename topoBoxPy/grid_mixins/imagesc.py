46fa822d58201c8058c7cd693b05409e57a12f9d
Teschl <69400012+Teschl@users.noreply.github.com>
2024-03-04 15:27:53 +0100

    imagesc and imageschs working (some functionality missing)

--- topoBoxPy/grid_mixins/imagesc.py
+++ topoBoxPy/grid_mixins/imagesc.py
@@ -3,14 +3,12 @@ from matplotlib import colormaps
 
 class ImagescMixin:
     def imagesc(self, **kwargs):
-        # select colormap from plt colormaps
-        if 'colormap' in kwargs:
-            if kwargs['colormap'] in colormaps:
-                colormap = kwargs['colormap']
-            else:
-                raise ValueError("Choose from the matplotlib colormap options.")
-        else:
-            colormap = 'gist_earth'
+        # Get inputs from kwargs
+        colormap = kwargs.get('colormap', "terrain")
+
+        # Type checking inputs
+        if not isinstance(colormap, str) or colormap not in colormaps:
+            raise ValueError("colrmap must be a string chosen from the matplotlib colormap variants.")
 
         plt.imshow(self.z, cmap=colormap, interpolation='nearest')
         plt.colorbar()
--- topoBoxPy/grid_mixins/imageschs.py
+++ topoBoxPy/grid_mixins/imageschs.py
@@ -1,95 +1,88 @@
 import numpy as np
 import matplotlib.pyplot as plt
+# import matplotlib.colors as mcolors
 from matplotlib.colors import LightSource
 from matplotlib import colormaps
 
+
 class ImageschsMixin:
     def imageschs(self, **kwargs):
-
-        if 'caxis' in kwargs:
-            caxis = kwargs['caxis']
-
-        if 'colorbar' in kwargs:
-            if isinstance(kwargs['colorbar'], bool):
-                add_colorbar = kwargs['colorbar']
-            else:
-                raise ValueError("colorbar kwargs expects boolean.")
-        else:
-            add_colorbar = True
-
-        # to be added
-        if 'colorbarlabel' in kwargs:
-            colorbarlabel = kwargs['colorbarlabel']
-        if 'colorbarylabel' in kwargs:
-            colorbarylabel = kwargs['colorbarylabel']
-            
-        # select colormap from plt colormaps
-        if 'colormap' in kwargs:
-            if kwargs['colormap'] in colormaps:
-                colormap = kwargs['colormap']
-            else:
-                raise ValueError("Choose from the matplotlib colormap options.")
-        else:
-            colormap = 'gist_earth'
-
-        if 'percentclip' in kwargs:
-            percentclip = kwargs['percentclip']
-        truecolor = [0,1,0]
-        if 'truecolor' in kwargs:
-            truecolor = kwargs['truecolor']
-        falsecolor = [1,1,1]
-        if 'falsecolor' in kwargs:
-            falsecolor = kwargs['falsecolor']
-        nancolor = [1,1,1]
-        if 'nancolor' in kwargs:
-            nancolor = kwargs['nancolor']
-        brighten = 0
-        if 'brighten' in kwargs:
-            brighten = kwargs['brighten']
-        # important!
-        usepermanent = False
-        if 'usepermanent' in kwargs:
-            usepermanent = kwargs['usepermanent']
-        medfilt = False
-        if 'medfilt' in kwargs:
-            medfilt = kwargs['medfilt']
-        # can be removed?
-        method = 'surfnorm'
-        if 'method' in kwargs:
-            method = kwargs['method']
-
-        # set azimuth and altitude 
-        azimuth = 315
-        if 'azimuth' in kwargs:
-            azimuth = kwargs['azimuth']
-        altitude = 60
-        if 'altitude' in kwargs:
-            altitude = kwargs['altitude']
-        ls = LightSource(azdeg=azimuth, altdeg=altitude)
+        # Get inputs from kwargs:
+        # caxis = kwargs.get()
+        colorbar = kwargs.get("colorbar",True)
+        colorbarlabel = kwargs.get("colorbarlabel", "")
+        colorbarylabel = kwargs.get("colorbarylabel", "")
+        colormap = kwargs.get('colormap', 'gist_earth')
+        percentclip = kwargs.get("percentclip", 0)
+        truecolor = kwargs.get("truecolor", "g")
+        falsecolor = kwargs.get("falsecolor", "w")
+        nancolor = kwargs.get("nancolor", "w")
+        brighten = kwargs.get("brighten", 0)
+        usepermanent = kwargs.get("usepermanent", False)
+        medfilt = kwargs.get("medfilt", False)
+        # method = kwargs.get()
+        azimuth = kwargs.get("azimuth", 315)
+        altitude = kwargs.get("altitude", 60)
+        exaggerate = kwargs.get("exaggerate", 2)
+        # ticklabels = kwargs.get()
+        # tickstokm = kwargs.get()
+        # gridmarkers = kwargs.get()
+        # gridmarkercolor = kwargs.get()
         
-        exaggerate = 2
-        if 'exaggerate' in kwargs:
-            exaggerate = kwargs['exaggerate']
+        # Type checking inputs:
+        #if not isinstance(caxis, ):
+        #    raise ValueError("")
+        if not isinstance(colorbar, bool):
+            raise ValueError("Colorbar must be a boolean value.")
+        if not isinstance(colorbarlabel, str):
+            raise ValueError("Colorbarlabel must be a string.")
+        if not isinstance(colorbarylabel, str):
+            raise ValueError("Colorbarylabel must be a string.")
+        if not isinstance(colormap, str) or colormap not in colormaps:
+            raise ValueError("Colormap must be a string chosen from the matplotlib colormap variants.")
+        if not isinstance(percentclip, (int,float)):
+            raise ValueError("Percentclip must be a numeric value.")
+        if not isinstance(truecolor, str):
+            raise ValueError("Truecolor must be a string.")
+        if not isinstance(falsecolor, str):
+            raise ValueError("Falsecolor must be a string.")
+        if not isinstance(nancolor, str):
+            raise ValueError("Nancolor must be a string.")
+        if not isinstance(brighten, (int, float)):
+            raise ValueError("Brighten must be numeric value.")
+        if not isinstance(usepermanent, bool):
+            raise ValueError("Usepermanent must be a boolean value.")
+        if not isinstance(medfilt, bool):
+            raise ValueError("Medfilt must be a boolean value.")
+        # if not isinstance(method, str):
+        #     raise ValueError("")
+        if not isinstance(azimuth, (int, float)):
+            raise ValueError("Azimuth must be a numeric value.")
+        if not isinstance(altitude, (int, float)):
+            raise ValueError("Altitude must be a numeric value.")
+        if not isinstance(exaggerate, (int, float)):
+            raise ValueError("Exaggerate must be a numric value.")
+        # if not isinstance(ticklabels, ):
+        #     raise ValueError("")
+        # if not isinstance(tickstokm, ):
+        #     raise ValueError("")
+        # if not isinstance(gridmarkers, ):
+        #     raise ValueError("")
+        # if not isinstance(gridmarkercolor, ):
+        #     raise ValueError("")
+        
+        # Plotting the DEM:
+        ls = LightSource(azdeg=azimuth, altdeg=altitude)
         hillshade = ls.hillshade(self.z, vert_exag=exaggerate)
 
-        # missing!
-        if 'ticklabels' in kwargs:
-            ticklabels = kwargs['ticklabels']
-        # missing!
-        if 'tickstokm' in kwargs:
-            tickstokm = kwargs['tickstokm']
-        # missing!
-        if 'gridmarkers' in kwargs:
-            gridmarkers = kwargs['gridmarkers']
-        # missing!
-        if 'gridmarkercolor' in kwargs:
-            gridmarkercolor = kwargs['gridmarkercolor']
+        img = plt.imshow(self.z, cmap=colormap, interpolation='nearest')
+        img_overlay = plt.imshow(hillshade,  cmap="gray", alpha=0.5)
+        if colorbar:
+            cbar = plt.colorbar(img)
+            if not colorbarlabel == "":
+                cbar.set_label(colorbarlabel)
+            if not colorbarylabel == "":
+                cbar.set_label(colorbarylabel, rotation=270, labelpad=15)
 
-        plt.imshow(hillshade, cmap=colormap, interpolation='nearest')  
-
-        if add_colorbar:
-            plt.colorbar()
-                
-
+        # Show plot
         plt.show()
-            
